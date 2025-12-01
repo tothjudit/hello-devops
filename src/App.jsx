82c55f9bc053e520eadbe027a59ee86f7d9422a5
Tóth Judit <tothjudit.ditta@gmail.com>
2025-12-01 11:40:43 +0000

    Update greeting message on feature branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "3rem" }}>
       <h1>Hello DevOps 👋</h1>
-      <p>Ez az én DevOps beadandó projektem.</p>
+      <p>Ez a feature branch módositott üzenete.</p>
     </div>
   );
 }
