d5b569a732a43293fad963bb553f5b903b8c1b57
Tóth Judit <tothjudit.ditta@gmail.com>
2025-12-01 11:16:43 +0000

    Initialize Hello DevOps React app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "3rem" }}>
+      <h1>Hello DevOps 👋</h1>
+      <p>Ez az én DevOps beadandó projektem.</p>
+    </div>
+  );
+}
+
+export default App;
